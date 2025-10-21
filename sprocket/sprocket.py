1f9ab27895b3b8f74cc23e2236c76ecb9c1425dc
technillogue <technillogue@gmail.com>
2025-10-21 11:54:08 -0400

    factor out queue client (#29)

--- sprocket/sprocket.py
+++ sprocket/sprocket.py
@@ -55,6 +55,79 @@ class FileOutput(pathlib.PosixPath):
     "Output file to be uploaded"
 
 
+class QueueClient:
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+        api_key = os.getenv("TOGETHER_API_KEY")
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {api_key}",
+        }
+        if VERSION:
+            headers["x-worker-version"] = VERSION
+        self.client = httpx.AsyncClient(headers=headers, timeout=None)
+
+    async def get_job(self) -> dict:
+        params = {"timeout": "5s", "model": self.model_name, "hostname": HOSTNAME}
+        start_time = time.time()
+        while not SHUTDOWN_REQUESTED.exists():
+            try:
+                logger.info(f"waiting for job from remote queue {self.model_name}")
+                headers = {"x-idle-time": str(time.time() - start_time)}
+                response = await self.client.get(
+                    RETRIEVE_URL, headers=headers, params=params
+                )
+                response.raise_for_status()
+                resp = response.json()
+                if resp.get("data"):
+                    return resp["data"]
+                logger.info("no job found in remote queue")
+            except httpx.HTTPError as e:
+                logger.error(f"failed to get job from remote queue: {repr(e)}")
+            await asyncio.sleep(random.random() / 5)
+        raise Exception("exit requested")
+
+    async def update_job_status(
+        self,
+        request_id: str,
+        status: str,
+        outputs: Optional[dict] = None,
+        info: Optional[dict] = None,
+    ) -> bool:
+        data = {
+            "model": self.model_name,
+            "request_id": request_id,
+            "status": status,
+            "payload": outputs or {},
+        }
+        if info:
+            data["info"] = info
+        logger.info(f"updating remote job status: {data}")
+        for i in range(3):
+            try:
+                response = await self.client.post(UPDATE_URL, json=data)
+                if response.status_code == 200:
+                    return True
+                response.raise_for_status()
+            except httpx.HTTPError as e:
+                logger.error(f"failed to update job status: {e}")
+        logger.error("failed to update job status after 3 retries")
+        return False
+
+    async def upload_file(self, request_id: str, path: FileOutput) -> str:
+        try:
+            resp = await self.client.post(
+                UPLOAD_URL, json={"filename": request_id + "-" + path.name}
+            )
+            await self.client.put(
+                resp.json()["upload_url"]["url"], content=path.open("rb").read()
+            )
+        except:
+            traceback.print_exc()
+            raise
+        return f"{API_BASE}/v1/storage/{request_id}-{path.name}"
+
+
 class OrjsonResponse(JSONResponse):
     def render(self, content: "Any") -> bytes:
         return orjson.dumps(content)
@@ -100,94 +173,23 @@ class AsyncSprocket:
 
 class Runner:
     def __init__(self, sprocket: Sprocket | AsyncSprocket, model_name: str) -> None:
-        api_key = os.getenv("TOGETHER_API_KEY")
-        self.headers = {
-            "Content-Type": "application/json",
-            "Authorization": f"Bearer {api_key}",
-        }
-        if VERSION:
-            self.headers["x-worker-version"] = VERSION
-        self.client = httpx.AsyncClient(headers=self.headers, timeout=None)
+        self.queue_client = QueueClient(model_name)
+        # borrow httpx client
+        self.download_client = httpx.AsyncClient(timeout=None, follow_redirects=True)
         self.sprocket = sprocket
         # create InputOutputProcessor, potentially overriden by sprocket
         self.io_processor = sprocket.processor()
-        self.model_name = model_name
         self.busy = False
         self.queue_mode = False
         self.healthy = False
 
-    async def get_job(self) -> dict:
-        params = {"timeout": "5s", "model": self.model_name, "hostname": HOSTNAME}
-        start_time = time.time()
-        while not SHUTDOWN_REQUESTED.exists():
-            try:
-                logger.info(f"waiting for job from remote queue {self.model_name}")
-                headers = {"x-idle-time": str(time.time() - start_time)}
-                response = await self.client.get(
-                    RETRIEVE_URL, headers=headers, params=params
-                )
-                response.raise_for_status()
-                resp = response.json()
-                if resp.get("data"):
-                    return resp["data"]
-                logger.info("no job found in remote queue")
-            except httpx.HTTPError as e:
-                logger.error(f"failed to get job from remote queue: {repr(e)}")
-            await asyncio.sleep(random.random() / 5)
-        raise Exception("exit requested")
-
     async def download_file(self, url: str) -> pathlib.Path:
         # replace with more sophisticated download later
         dst = pathlib.Path("inputs/" + os.path.basename(urlparse(url).path))
-        req = self.client.build_request("GET", url)
-        req.headers = httpx.Headers(
-            {k: v for k, v in req.headers.items() if k not in self.headers}
-        )
-        resp = await self.client.send(req, follow_redirects=True)
+        resp = self.download_client.get(url)
         resp.raise_for_status()
-
         self.io_processor.process_input_file(resp, dst)
         return dst
-
-    async def upload_file(self, request_id: str, path: FileOutput) -> str:
-        try:
-            resp = await self.client.post(
-                UPLOAD_URL, json={"filename": request_id + "-" + path.name}
-            )
-            await self.client.put(
-                resp.json()["upload_url"]["url"], content=path.open("rb").read()
-            )
-        except:
-            traceback.print_exc()
-            raise
-        return f"{API_BASE}/v1/storage/{request_id}-{path.name}"
-
-    async def update_job_status(
-        self,
-        request_id: str,
-        status: str,
-        outputs: Optional[dict] = None,
-        info: Optional[dict] = None,
-    ) -> bool:
-        data = {
-            "model": self.model_name,
-            "request_id": request_id,
-            "status": status,
-            "payload": outputs or {},
-        }
-        if info:
-            data["info"] = info
-        logger.info(f"updating remote job status: {data}")
-        for i in range(3):
-            try:
-                response = await self.client.post(UPDATE_URL, json=data)
-                if response.status_code == 200:
-                    return True
-                response.raise_for_status()
-            except httpx.HTTPError as e:
-                logger.error(f"failed to update job status: {repr(e)}")
-        logger.error("failed to update job status after 3 retries")
-        return False
 
     async def handle_job(self, inputs: dict, request_id: str) -> dict:
         logger.info(f"queue inputs: {inputs}")
@@ -204,7 +206,8 @@ class Runner:
                 output = await self.sprocket.predict(inputs)
             else:
                 output = self.sprocket.predict(inputs)
-            logger.info("total predict run time: ", time.time() - job_start_time)
+            logger.info("total predict run time: {time.time() - job_start_time}")
+            output = await self.io_processor.finalize(request_id, inputs, output)
             return {
                 k: await self.upload_file(request_id, v)
                 if isinstance(v, FileOutput)
@@ -217,17 +220,21 @@ class Runner:
                 path.unlink(missing_ok=True)
 
     async def run_one_job(self) -> None:
-        job = await self.get_job()
+        job = await self.queue_client.get_job()
         request_id = job["request_id"]
         try:
             output = await self.handle_job(job["payload"], request_id)
         except Exception as e:
             traceback.print_exc()
             logger.error(f"Job {request_id} failed")
-            await self.update_job_status(request_id, "failed", info={"error": repr(e)})
+            await self.queue_client.update_job_status(
+                request_id, "failed", info={"error": repr(e)}
+            )
         else:
             logger.info(f"Job {request_id} finished")
-            await self.update_job_status(request_id, "done", outputs=output)
+            await self.queue_client.update_job_status(
+                request_id, "done", outputs=output
+            )
 
     async def run_queue_worker(self) -> None:
         try:
