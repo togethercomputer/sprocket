5b905603e8f8ace51c7d3a88f76d4ac262f43107
Dulaj Disanayaka <ddisanayaka@together.ai>
2026-04-08 15:11:53 +0200

    sync: update from internal 2026-04-08

--- jig.py
+++ jig.py
@@ -1,24 +1,11 @@
-#!/usr/bin/env python3
-"""jig - Simple deployment tool for Together AI"""
-
+#!/usr/bin/env -S uv run
 # /// script
 # requires-python = ">=3.11"
-# dependencies = ["together @ git+https://github.com/togethercomputer/together-py@next"]
+# dependencies = ["together @ git+https://github.com/togethercomputer/together-py@v2.2.1-alpha0" ]
 # ///
-import click
-from together import Together
+"""jig - Simple deployment tool for Together AI"""
 
-from together.lib.cli.api.beta.jig import add_commands
-
-
-@click.group()
-@click.pass_context
-def jig(ctx: click.Context) -> None:
-    """Jig - deployment tool for Together AI"""
-    ctx.obj = Together()
-
-
-add_commands(jig)
+from together.lib.cli.api.beta.jig import jig
 
 if __name__ == "__main__":
     jig()
